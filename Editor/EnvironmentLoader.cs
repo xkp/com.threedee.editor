7230ec4cc00d2653ee2d4cc70a6652caaa7cb736
xkp <xkp.santos@gmail.com>
2025-04-26 20:52:33 -0400

    environement loader

---
+++ Editor/EnvironmentLoader.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EnvironmentLoader : EditorWindow
+{
+	private string inputFolderPath = "";
+	private string outputFolderPath = "";
+
+	[MenuItem("Tools/Environment Loader")]
+	public static void OpenWindow()
+	{
+		GetWindow<EnvironmentLoader>("Environment Loader");
+	}
+
+	private void OnGUI()
+	{
+		GUILayout.Label("Select Folders", EditorStyles.boldLabel);
+
+		if (GUILayout.Button("Choose Input Folder"))
+		{
+			string path = EditorUtility.OpenFolderPanel("Select Input Folder", "", "");
+			if (!string.IsNullOrEmpty(path))
+			{
+				inputFolderPath = path;
+			}
+		}
+
+		if (!string.IsNullOrEmpty(inputFolderPath))
+		{
+			GUILayout.Label("Input Folder:");
+			GUILayout.TextField(inputFolderPath);
+		}
+
+		GUILayout.Space(5);
+
+		if (GUILayout.Button("Choose Output Folder"))
+		{
+			string path = EditorUtility.OpenFolderPanel("Select Output Folder", "", "");
+			if (!string.IsNullOrEmpty(path))
+			{
+				outputFolderPath = path;
+			}
+		}
+
+		if (!string.IsNullOrEmpty(outputFolderPath))
+		{
+			GUILayout.Label("Output Folder:");
+			GUILayout.TextField(outputFolderPath);
+		}
+
+		GUILayout.Space(10);
+
+		GUI.enabled = !string.IsNullOrEmpty(inputFolderPath) && !string.IsNullOrEmpty(outputFolderPath);
+		if (GUILayout.Button("Accept"))
+		{
+			OnFoldersAccepted(inputFolderPath, outputFolderPath);
+		}
+		GUI.enabled = true;
+	}
+
+	// This is where your logic will go
+	private void OnFoldersAccepted(string inputFolder, string outputFolder)
+	{
+		ThreedeeLoader.Load(inputFolder, ouputFolder);
+	}
+}
